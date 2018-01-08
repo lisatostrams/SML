e08fd8cd540c7926e8d2d25dc806b0356f391d14
Daniele Maccari <daniele.maccari@gmail.com>
2018-01-08 18:13:32 +0100

    Added ex. 4 up to question 5

---
+++ Assignment 4/kernel.m
@@ -0,0 +1,11 @@
+function f = kernel(theta, x, x_prime)
+    theta_0 = theta(1);
+    theta_1 = theta(2);
+    theta_2 = theta(3);
+    theta_3 = theta(4);
+    
+    norm_2 = (x-x_prime)'*(x-x_prime);
+    
+    f = theta_0 * exp(-(theta_1/2)*norm_2) + theta_2 + theta_3 * (x'*x_prime);  
+end
+
