12d82ddab61056532a96a445cb07ddfeb63773d3
Daniele Maccari <daniele.maccari@gmail.com>
2018-01-11 16:57:33 +0100

    Added code for ex. 3
    Up to the plot of the approximation steps. Still need to try different params and classes.

--- Assignment 4/Bernoulli_EM.m
+++ Assignment 4/Bernoulli_EM.m
@@ -1,6 +1,74 @@
-function f = Bernoulli_EM(X,mu,pi)
-    bern = @(x,p) p^x .* (1-p)^(1-x);
-    p_X_mu = arrayfun(bern, X, mu) 
-    p_X_mu_pi = sum(pi*prod(p_X_mu));
+function f = Bernoulli_EM(X, mu, pi, iterations)
+    mu_ = mu; pi_ = pi;
+    K = size(mu_,1);
+    D = size(mu_,2);
+    it = zeros(iterations,K,D+1);
+    
+    for i=1:iterations
+        gamma = expectation(X,mu_,pi_);
+        [mu_, pi_] = maximization(X,gamma);
+        
+%         % map means to grayscale
+%         mu__ = uint8(mu_ * 255);
+%         
+%         figure();
+%         colormap(gray(255));
+%         for k=1:K
+%             subplot(1,K,k);
+%             image(reshape(mu__(k,:),[28,28]));
+%             title(sprintf('Class: %d', k));        
+%         end
+        
+        it(i,:,:) = [mu_, pi_];
+    end
+    
+    f = it;
 end
 
+function f = expectation(X, mu, pi)
+    N = size(X,1);
+    K = size(pi,1);
+        
+    bern = @(x,p) (p.^x .* (1-p).^(1-x));
+    gamma = zeros(N,K);
+    p_X_mu = zeros(N,K);
+    for n=1:N
+        for k=1:K
+            p_xn_muk = exp(sum(log(bern(double(X(n,:)), mu(k,:)))));
+            gamma(n,k) = (pi(k) .* p_xn_muk);
+            p_X_mu(n,k) = p_xn_muk;
+        end
+    end
+    
+%     log_pi = log(pi);
+%     log_mu = log(mu);    
+%     % array of expectations for the log likelhoods of each image
+%     C = X * log_mu' + (1-X) * (1-log_mu)'; % N x K
+%     
+%     expectations = zeros(N,1);
+    normalizer = (p_X_mu * pi); % N x K, K x 1 -> N x 1
+    for n=1:N
+        gamma(n,:) = gamma(n,:) ./ normalizer(n);
+%         expectations(n) = gamma(n,:)*(log_pi + C(n,:));
+    end
+       
+    f = gamma;
+end
+
+function [mu_, pi_] = maximization(X, gamma)
+    K = size(gamma,2);
+    N = size(X,1);
+    D = size(X,2);
+    mu = zeros(K,D);
+    pi = zeros(K,1);
+    
+    for k=1:K
+        N_k = sum(gamma(:,k));
+        for d=1:D
+            mu(k,d) = (sum(gamma(:,k) .* double(X(:,d)))) / N_k;
+        end
+        pi(k) = N_k / N;
+    end
+    
+    mu_ = mu; pi_ = pi;
+end
--- Assignment 4/EM_mnist.m
+++ Assignment 4/EM_mnist.m
@@ -1,23 +1,50 @@
 %% load the data
 N = 800; 
-D = 28*28; 
-X = uint8(zeros(N,D)); 
+D = 28*28;
+K = 3;
+X = uint8(zeros(N,D));
 fid = fopen('a012_images.dat', 'r'); 
-for i = 1:N 
-    X(i,:) = fread(fid, [D], 'uint8'); 
+for step = 1:N 
+    X(step,:) = fread(fid, D, 'uint8'); 
 end 
 status = fclose(fid);
 
 %% show some sample images
 % for reproducibility
-rng(42);
+% rng(42);
 ix = randi(N,9,1);
 samples = X(ix,:);
 
 BW_map=[1,1,1; 0,0,0]; colormap(BW_map);
 hold on;
-for i=1:size(samples,1)
-    subplot(3,3,i);
-    image(reshape(X(i,:),[28,28]));
+for s=1:size(samples,1)
+    subplot(3,3,s);
+    image(reshape(X(s,:),[28,28]));
 end
 hold off;
+
+%% run the algorithm, try different initializations
+for i=1:3
+    mu = random('unif', 0.25, 0.75, [K,D]);
+    pi = ones(K,1)/K;
+
+    it = Bernoulli_EM(X,mu,pi,40);
+
+    %% display the result
+    figure();
+    colormap(gray(255));    
+    for step=1:4
+        mu = squeeze(it(step,:,1:D));
+        pi = squeeze(it(step,:,end));
+
+        % map means to grayscale
+        mu = uint8(mu * 255);
+        
+        for k=1:K
+            subplot(4,K,(step-1)*K+k);
+            image(reshape(mu(k,:),[28,28]));
+            title(sprintf('Class: %d', k));        
+        end
+        
+    end
+end
